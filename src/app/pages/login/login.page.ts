1f4481070e7cdb670109f37fba12c09965ec802e
mahoga826 <mahoga26kathyani@gmail.com>
2020-10-22 07:27:38 +0530

    login and register done

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router,ActivatedRoute } from '@angular/router';
+import { ToastController,LoadingController,AlertController } from '@ionic/angular';
+import { AccessProviders } from '../../pro/access';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +9,18 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+ // username:string="";
+  telephone_number:string="";
+  password:string="";
 
-  constructor(private router :Router) { 
-    
-  }
+  disableButton;
 
+  constructor(private router:Router,
+    private toastCtrl:ToastController,
+    private loadingCtrl:LoadingController,
+    private alertCtrl:AlertController,
+    private acessPr:AccessProviders,
+) { }
   ngOnInit() {
   }
 
@@ -19,8 +28,82 @@ export class LoginPage implements OnInit {
     this.router.navigate(['/register']);
   }
 
-  tryLogin(){
-    this.router.navigate(['/farmer-profile']);
+ async tryLogin(){
+    //this.router.navigate(['/farmer-profile']);
+   if(this.telephone_number==""){
+    this.presentToast("Telephone number is required");
+  }else if(this.password=="")
+  {
+    this.presentToast("Password is required");
   }
+  else{
+    this.disableButton=true;
+    const loader=await this.loadingCtrl.create({
+        message:'Please wait......',
+    });
+    loader.present();
+      return new Promise(resoler=>{
+        let body={
+         
+          
+          telephone_number:this.telephone_number,
+          password:this.password,
+         
+        }
+        this.acessPr.postLogin(body).subscribe((res:any)=>{
+            if(res.status==true){
+              loader.dismiss();
+              this.disableButton=false;
+              this.presentToast(res.message);
+              this.router.navigate(['/farmer-profile']);
 
+            }else{
+              loader.dismiss();
+              this.disableButton=false;
+              this.presentToast(res.message);
+            }
+        },(err=>{
+          loader.dismiss();
+          this.disableButton=false;
+          this.presentAlert('Timeout');
+        }));
+      });
+
+    
+  }
 }
+async presentToast(a) {
+  let toast = await this.toastCtrl.create({
+    message: a,
+    duration: 3000,
+    position: 'top'
+  });
+toast.present();
+}
+
+async presentAlert(a) {
+  const alert = await this.alertCtrl.create({
+   backdropDismiss:false,
+    header: a,
+    
+    buttons: [
+      {
+        text: 'close',
+        handler: (blah) => {
+          console.log('Confirm Cancel: blah');
+        }
+      }, {
+        text: 'Try Again',
+        handler: () => {
+         this.tryLogin()
+        }
+      }
+    ]
+  });
+
+  await alert.present();
+}
+
+  }
+  
+
--- src/app/pro/access.ts
+++ src/app/pro/access.ts
@@ -4,6 +4,15 @@ import {HttpClient,HttpHeaders,HttpErrorResponse}  from '@angular/common/http';
 import 'rxjs/add/operator/timeout';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs';
+export class User {
+    
+  telephone_number: string;
+
+  constructor( telephone_number: string) {
+   
+    this.telephone_number = telephone_number;
+  }
+}
 
 
 
@@ -15,7 +24,8 @@ import { Observable } from 'rxjs';
 
 export class AccessProviders{
  server:string='http://localhost:8000';
-
+ currentUser: User;
+ isLogged: Boolean = false;
 
    constructor(
        
@@ -31,13 +41,31 @@ export class AccessProviders{
                  headers:headers
             }
             
-            return this.http.post(this.server+'/register',JSON.stringify(body),{
-                headers: new HttpHeaders().set('Content-Type', 'applicationJson,charset-UTF-8'),
+            return this.http.post(this.server+'/api/farmers',JSON.stringify(body),{
+                headers: new HttpHeaders().set('Content-Type', 'application/json'),
               }).timeout(59000)
             . map(res=>res);
             
             
         }
+
+        postLogin(body){
+          let headers=new HttpHeaders({
+              'Content-Type':'applicationJson,charset-UTF-8'
+          });
+          let options={
+              headers:headers
+          }
+          this.currentUser = new User(body.telephone_number);
+          this.isLogged = true;
+          return this.http.post(this.server+'/login',JSON.stringify(body),{
+              headers: new HttpHeaders().set('Content-Type', 'application/json'),
+            })
+          . map(res=>res
+          );
+          
+          
+      }
        
         
 }
